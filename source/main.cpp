e61e0eda70d2bbac6f4bc1828a94e2dbc818f54f
ManiacOfGitHub <43019219+ManiacOfGitHub@users.noreply.github.com>
2021-11-09 08:56:03 -0500

    Test

--- source/main.cpp
+++ source/main.cpp
@@ -5,17 +5,19 @@ int main(int argc, char **argv) {
     gfxInitDefault();
     consoleInit(GFX_TOP, NULL);
 
-	printf("Test Code::Blocks project!");
+	printf("Copy Save Data from Game Card to Digital Title\nMade by ManiacOfGitHub");
+	
 
-	printf("\x1b[20;15HPress Start to exit.");
+	printf("\x1b[20;15HPress L+A to copy or push Start to exit.");
 
     while(aptMainLoop()) {
         gspWaitForVBlank();
         hidScanInput();
 
-        if(hidKeysDown() & KEY_START)
-            break;
-
+        if(hidKeysDown() & KEY_START) break;
+	if(hidKeysDown() & KEY_A && hidKeysDown & KEY_L) {
+		printf("\x1b[1;1HThis is where it would copy the code if I wasn't so bad at programming in C++.");
+	}
         gfxFlushBuffers();
         gfxSwapBuffers();
     }
